227ddbacdca92ffe671d7a6cfc5af9cc0e1e24e1
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-16 22:44:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,6 +26,16 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+      { :railroads => 
+    {pieces: 4 , 
+    rent_in_dollars: {
+      
+    } , 
+    names: {} 
+      
+    }
+      
+    }
 
   
   # When you start writing the implementation for this method, copy the Hash
